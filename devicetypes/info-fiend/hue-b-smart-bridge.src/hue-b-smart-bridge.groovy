7e06a2acb1bff430d81ee67bb7f8fa7919da42a5
Steven Dale <dale.steven@gmail.com>
2017-05-15 01:23:45 +0100

    Update hue-b-smart-bridge.groovy

--- devicetypes/info-fiend/hue-b-smart-bridge.src/hue-b-smart-bridge.groovy
+++ devicetypes/info-fiend/hue-b-smart-bridge.src/hue-b-smart-bridge.groovy
@@ -73,7 +73,7 @@ def updated() {
 
 def initialize() {
 	def commandData = parent.getCommandData(device.deviceNetworkId)
-    log.debug "Line 76 ${commandData = commandData}"
+    log.debug "Line 76 ${commandData}"
     sendEvent(name: "idNumber", value: commandData.deviceId, displayed:true, isStateChange: true)
     sendEvent(name: "networkAddress", value: commandData.ip, displayed:false, isStateChange: true)
     sendEvent(name: "username", value: commandData.username, displayed:false, isStateChange: true)
